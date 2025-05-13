85a9a497ef545ebe9da32ffd1911f3ad2aad8314
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-05-12 21:40:33 -0300

    Criação das rotas de serviços

--- app/Http/Controllers/servicoController.php
+++ app/Http/Controllers/servicoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class servicoController extends Controller
+class ServicoController extends Controller
 {
     /**
      * Display a listing of the resource.
