67f50cc680d21d61d3c8bd23228445162b7ef9bb
gabreudev <biel0alves0@gmail.com>
2025-06-07 11:24:22 -0300

    feat: add ServicoEnum

---
+++ app/Enums/Servico.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Enums;
+
+enum ServicoEnum: string
+{
+    case CULINARIA = 'culinaria';
+    case REPAROS = 'reparos';
+    case PINTURA = 'pintura';
+    case LIMPEZA = 'limpeza';
+    case MUDANCA = 'mudanca';
+    case PISCINA = 'piscina';
+    case OUTROS = 'outros';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::CULINARIA => 'culinaria',
+            self::REPAROS => 'Reparos',
+            self::PINTURA => 'pintura',
+            self::LIMPEZA => 'limpeza',
+            self::MUDANCA => 'mudança',
+            self::PISCINA => 'piscina',
+            self::OUTROS => 'outros',
+        };
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
